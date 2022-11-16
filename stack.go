0f406ad75ebf8608c46f488e3c1c178a28ff0818
Prameesh <prameepramee0@gmail.com>
2022-11-16 06:30:06 +0530

    extra comments

--- stack.go
+++ stack.go
@@ -1,17 +1,28 @@
 package main
 
 import "fmt"
+
 //stack struct created
+
 type stack struct {
+	
 	values []int
+
 }
 //method to push values
+
 func (s *stack) push(val int) {
+	
 	s.values = append(s.values, val)
+
 }
+
 func main() {
+	//assingining stack to empty  stack variable 
+	//short variable decleration
 	Mystack := stack{}
-	fmt.Println(Mystack)
+	fmt.Println(Mystack)//printing empty stack
+	//pushing values in the slice
 	Mystack.push(10)
 	Mystack.push(20)
 	Mystack.push(30)
@@ -19,13 +30,23 @@ func main() {
 	Mystack.push(50)
 	Mystack.push(60)
 	fmt.Println(Mystack)
+//Mystack.pop() this call go to the method pop. And deletion will work.
 	Mystack.pop()
 	fmt.Println(Mystack)
 	Mystack.pop()
 	fmt.Println(Mystack)
+
 }
-func (s *stack) pop()int  {
+
+//method to remove elementes from the stack(slice)..
+
+func (s *stack) pop()int {
+	
+//The variable for store already deleted elements to store.
+//and the values is return to the pop method.
 	toremove:=s.values[len(s.values)-1]
+//s.values for delete elements
 	s.values=s.values[:len(s.values)-1]
 	return toremove
+
 }
